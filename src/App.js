4a9f02005ab0fba1dfc3bd6ce65c4dd948fa011d
“Tran <tranvanlong6677@gmail.com>
2023-04-17 11:20:54 +0700

    fetch data nouse redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 import {
   increment,
   decrement,
   incrementByAmount,
 } from "./redux/slices/counterSlice";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [listUsers, setListUsers] = useState([]);
   const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
+
+  const fetchAllUsers = async () => {
+    let res = await axios.get("http://localhost:8080/users/all");
+    console.log(">>> check res", res.data);
+    setListUsers(res.data ? res.data : []);
+  };
+  useEffect(() => {
+    fetchAllUsers();
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
-        <input type="text" />
         <button onClick={() => dispatch(increment())}>Increase</button>
         <button onClick={() => dispatch(decrement())}>Decrease</button>
         <button onClick={() => dispatch(incrementByAmount)}>TL</button>
         <div>Count = {count}</div>
+        <div className="table-user">
+          <table>
+            <thead>
+              <th>ID</th>
+              <th>Email</th>
+              <th>Username</th>
+            </thead>
+            <tbody>
+              {listUsers &&
+                listUsers.length > 0 &&
+                listUsers.map((item,index) => {
+                  return (
+                    <tr key={`${index}-item`}>
+                      <td>{item.id}</td>
+                      <td>{item.email}</td>
+                      <td>{item.username}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
--- src/redux/slices/counterSlice.js
+++ src/redux/slices/counterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: 0,
-};
+};  
 
 export const counterSlice = createSlice({
   name: "counter",
